7c2062e5d13ecf10ba8750dfcbc23706997634e5
Kevin Ure <kevin.ure@accenture.com>
2020-03-09 13:35:26 -0400

    should be fixed now

--- target-toolchain.cmake
+++ target-toolchain.cmake
@@ -18,7 +18,7 @@ set(TI_CGT_BIN_PATH ${TI_CGT_PATH}/bin/ CACHE INTERNAL "")
 # specify the cross compiler
 set(CMAKE_C_COMPILER ${TI_CGT_BIN_PATH}/armcl)
 set(CMAKE_CXX_COMPILER ${TI_CGT_BIN_PATH}/armcl)
-# set(CMAKE_ASM_COMPILER ${TI_CGT_BIN_PATH}/armasm)
+set(CMAKE_ASM_COMPILER ${TI_CGT_BIN_PATH}/armcl)
 set(CMAKE_ASM_COMPILER ${TI_CGT_BIN_PATH}/armcl)
 # set(CMAKE_AR ${TI_CGT_BIN_PATH}/armar)
 # set(CMAKE_LINKER ${TI_CGT_BIN_PATH}/armlnk)
@@ -50,10 +50,10 @@ link_directories(${TI_CGT_PATH}/lib)
 # set target environment
 set(CMAKE_FIND_ROOT_PATH ${TI_CGT_PATH})
 # "C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/bin/armcl" -mv7R5 --code_state=32 --float_support=VFPv3D16 -g --diag_warning=225 --diag_wrap=off --display_error_number --enum_type=packed --abi=eabi -z -m"TMS570LC43_RTOS_LEDs.map" --heap_size=0x800 --stack_size=0x800 -i"C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/lib" -i"C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include" --reread_libs --diag_wrap=off --display_error_number --warn_sections --xml_link_info="TMS570LC43_RTOS_LEDs_linkInfo.xml" --rom_model --be32 -o "TMS570LC43_RTOS_LEDs.out" $(ORDERED_OBJS)
-set(PLATFORM_CONFIG_C_FLAGS "--c11 -mv7R5 --code_state=32 --float_support=VFPv3D16 -g --diag_warning=225 --diag_wrap=off --display_error_number --enum_type=packed --abi=eabi" CACHE STRING "platform config c flags")
+set(PLATFORM_CONFIG_C_FLAGS "--c11 -mv7R5 --code_state=32 --float_support=VFPv3D16 -g --diag_warning=225 --diag_wrap=off --display_error_number --enum_type=packed --abi=eabi --preproc_with_compile" CACHE STRING "platform config c flags")
 
 # combine flags to C and C++ flags
 SET(CMAKE_C_FLAGS "${PLATFORM_CONFIG_C_FLAGS} ${CMAKE_C_FLAGS}" CACHE STRING "platform config c flags")
 SET(CMAKE_CXX_FLAGS "${PLATFORM_CONFIG_CXX_FLAGS} ${CMAKE_CXX_FLAGS}" CACHE STRING "platform config c flags")
 SET(CMAKE_ASM_FLAGS ${CMAKE_C_FLAGS}  CACHE STRING "platform config asm flags")
-set(CMAKE_EXE_LINKER_FLAGS "-mv7R5 --define=DEBUG_PRINT --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi -z -m\"CCU_v2_Firmware.map\" --heap_size=0x800 --stack_size=0x800 --reread_libs --diag_wrap=off --display_error_number --warn_sections --xml_link_info=CCU_v2_Firmware_linkInfo.xml --rom_model --be32" CACHE STRING "platform config linker exe files")
+set(CMAKE_EXE_LINKER_FLAGS "-mv7R5 --define=DEBUG_PRINT --diag_warning=225 --diag_wrap=off --display_error_number --enum_type=packed --abi=eabi -z -m\"CCU_v2_Firmware.map\" --heap_size=0x800 --stack_size=0x800 --reread_libs --diag_wrap=off --display_error_number --warn_sections --xml_link_info=CCU_v2_Firmware_linkInfo.xml --rom_model --be32 " CACHE STRING "platform config linker exe files")
