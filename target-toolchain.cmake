633f25cf24e12c5d5c9ecbe2923d0e76c2884f0b
Kevin Ure <kevin.ure@accenture.com>
2020-03-09 10:08:34 -0400

    messing around

--- target-toolchain.cmake
+++ target-toolchain.cmake
@@ -9,12 +9,21 @@ else()
     message("TI_CGT_PATH environment variable is not defined!")
 endif()
 
+if(NOT CMAKE_FIND_ROOT_PATH)
+  set(CMAKE_FIND_ROOT_PATH ${TI_CGT_PATH})
+endif()
+
 set(TI_CGT_BIN_PATH ${TI_CGT_PATH}/bin/ CACHE INTERNAL "")
 
 # specify the cross compiler
+set(CMAKE_C_COMPILER ${TI_CGT_BIN_PATH}/armcl)
+set(CMAKE_CXX_COMPILER ${TI_CGT_BIN_PATH}/armcl)
+# set(CMAKE_ASM_COMPILER ${TI_CGT_BIN_PATH}/armasm)
+set(CMAKE_ASM_COMPILER ${TI_CGT_BIN_PATH}/armcl)
+# set(CMAKE_AR ${TI_CGT_BIN_PATH}/armar)
+# set(CMAKE_LINKER ${TI_CGT_BIN_PATH}/armlnk)
 
-SET(CMAKE_C_COMPILER ${TI_CGT_BIN_PATH}armcl)
-SET(CMAKE_ASM_COMPILER ${TI_CGT_BIN_PATH}armcl)
+SET(CMAKE_C_COMPILER_TARGET)
 
 # skip compiler tests
 set(CMAKE_C_COMPILER_FORCED TRUE)
@@ -40,6 +49,7 @@ link_directories(${TI_CGT_PATH}/lib)
 
 # set target environment
 set(CMAKE_FIND_ROOT_PATH ${TI_CGT_PATH})
+# "C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/bin/armcl" -mv7R5 --code_state=32 --float_support=VFPv3D16 -g --diag_warning=225 --diag_wrap=off --display_error_number --enum_type=packed --abi=eabi -z -m"TMS570LC43_RTOS_LEDs.map" --heap_size=0x800 --stack_size=0x800 -i"C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/lib" -i"C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include" --reread_libs --diag_wrap=off --display_error_number --warn_sections --xml_link_info="TMS570LC43_RTOS_LEDs_linkInfo.xml" --rom_model --be32 -o "TMS570LC43_RTOS_LEDs.out" $(ORDERED_OBJS)
 set(PLATFORM_CONFIG_C_FLAGS "--c11 -mv7R5 --code_state=32 --float_support=VFPv3D16 -g --diag_warning=225 --diag_wrap=off --display_error_number --enum_type=packed --abi=eabi" CACHE STRING "platform config c flags")
 
 # combine flags to C and C++ flags
